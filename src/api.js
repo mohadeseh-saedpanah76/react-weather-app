ffb93e464b69b95c651566950070aac978cc2791
saedpanah <m.saedpanah76@gmail.com>
2023-12-16 10:46:22 +0330

    changes2

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions';
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '45842899e0msh3493222403554a9p1d50cbjsn902a837f20ca',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,9 +1,28 @@
+import { response } from 'express'
 import React, {useState} from 'react'
 import { AsyncPaginate } from 'react-select-async-paginate'
+
+import { GEO_API_URL , geoApiOptions } from '../../api'
+
 
 
 const Search = ({onSearchChange}) => {
   const[search , setSearch] = useState(null)
+
+  const loadOptions = ()=> async(inputValue)=>{
+      // return fetch(`${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}` , geoApiOptions)
+      // .then(response => response.json())
+      // .then(response => console.log(response))
+      // .catch(err => console.error(err))
+
+      try {
+        const response = await fetch(`${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}` , geoApiOptions);
+        const result = await response.text();
+        console.log(result);
+      } catch (error) {
+        console.error(error);
+      }
+  }
 
   const handleOnChange = (searchData)=>{
     setSearch(searchData)
@@ -16,6 +35,7 @@ const Search = ({onSearchChange}) => {
       debounceTimeout={600}
       value={search}
       onChange={handleOnChange}
+      loadOptions={loadOptions}
     />
   )
 }
